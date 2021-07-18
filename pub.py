166c58019a102f785c833d625e938bf04ea9d27f
fede <info@fotovoltaica.com.ar>
2021-07-18 19:46:55 -0300

    pub.py first commit

--- pub.py
+++ pub.py
@@ -1,7 +1,11 @@
 import paho.mqtt.client as mqtt
 
 
-mqtt = mqtt.Client
+broker_address="138.36.239.25" 
+#broker_address="iot.eclipse.org" #use external broker
+client = mqtt.Client("P1") #create new instance
+client.connect(broker_address) #connect to broker
+client.publish("house/main-light","OFF") #publish
 
 
 
